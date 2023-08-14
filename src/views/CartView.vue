d1d204a84486fec7d0825e6931343bcb87e3f3e9
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-08-14 22:58:08 +0300

    58

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -69,7 +69,7 @@
             <br>
 
             <div v-for="(item, index) in cartItems" :key="index" class="cart-item">
-                <img :src="item.image" :alt="item.name" class="product-image">
+                <img src="/src/assets/images/about-img.png"  class="product-image">
                 <div class="product-details">
                     <h3>{{ item.name }}</h3>
                     <br>
